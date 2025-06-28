3551ea436d9d0550c6c15881eac65fe769e64a26
Oliver-coder-long <olishema11@gmail.com>
2025-06-28 17:07:30 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -24,7 +24,7 @@ CREATE TABLE Orders(
 );
 CREATE TABLE Order_Details(
   orderdetailid PRIMARY KEY
-  order_id FOREIGN KEY REFERENCING Orders TABLE
-  book_id FOREIGN KEY REFERENCING Books TABLE
+  order_id FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+  book_id INT FOREIGN KEY (book_id) REFERENCES Books(book_id)
   quantity DOUBLE
 );
