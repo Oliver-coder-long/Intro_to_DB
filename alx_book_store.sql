52ee66c87442838caf44eefdf500977eca97dd6c
Oliver-coder-long <olishema11@gmail.com>
2025-06-28 17:00:11 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,28 +1,28 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 SELECT alx_book_store;
-CREATE Books(
+CREATE TABLE Books(
   book_id PRIMARY KEY
   title VARCHAR(130)
   author_id FOREIGN KEY REFERENCING Authors table
   price DOUBLE
   publication_date DATE
 );
-CREATE Author(
+CREATE TABLE Author(
   author_id PRIMARY KEY
   author_name VARCHAR(215)
 );
-CREATE Customers(
+CREATE TABLE Customers(
   customer_id PRIMARY KEY
   customer_name VARCHAR(215)
   email VARCHAR(215)
   address TEXT
 );
-CREATE Orders(
+CREATE TABLE Orders(
   order_id PRIMARY KEY
   customer_id FOREIGN KEY REFERENCING Customers TABLE
   order_date DATE
 );
-CREATE Order_Details(
+CREATE TABLE Order_Details(
   orderdetailid PRIMARY KEY
   order_id FOREIGN KEY REFERENCING Orders TABLE
   book_id FOREIGN KEY REFERENCING Books TABLE
