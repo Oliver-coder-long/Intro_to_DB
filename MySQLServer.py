e73bbeb0aae7a48f843ecf6b616ef7916cff2c82
Oliver-coder-long <olishema11@gmail.com>
2025-06-29 14:32:52 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,3 +1,8 @@
 import mysql.connector
+mydb = mysql.connector.connect(
+  host =  "localhost"
+  user = "root"
+  password = "mysqlingoho1+"
+  database = "alx_book_store"
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 print("Database 'alx_book_store' created succesfully")
